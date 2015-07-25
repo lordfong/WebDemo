c5db2fdfcd40163085b860146a25d545d04f146d
JFong <lordfong@hotmail.com>
2015-07-25 23:52:14 +0200

    Implement Enity F ,Fix Repository and Models
    After implementing Entity Frame work, the repository and model were copied and now points to the webdemoapi instead of webdemo project.

--- WebDemoApi/Models/JapaneseWord.cs
+++ WebDemoApi/Models/JapaneseWord.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using WebDemo.DataAccessLayer;
+using WebDemoApi.DataAccessLayer;
 
 namespace WebDemoApi.Models
 {
--- WebDemoApi/Repository/JapaneseWordRepository.cs
+++ WebDemoApi/Repository/JapaneseWordRepository.cs
@@ -1,180 +1,182 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using WebDemo.DataAccessLayer;
-//using WebDemo.Models;
-//using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebDemoApi.DataAccessLayer;
+using WebDemoApi.Models;
+using System.Diagnostics;
 
-//namespace WebDemoApi.Repository
-//{
-//    /// <summary>
-//    /// I recall that repositories are usually static, but after researching why,
-//    /// I decided to make it none static so it can be tested in integration/unit test.
-//    /// 
-//    /// The feature to search by symbol or word isn't implemented yet, I am not sure how i would implement this yet on the front-end
-//    /// </summary>
-//    public class JapaneseWordRepository
-//    {
-//        private  WebDemoDataContext context;
-//        private EventLog appLog;
+namespace WebDemoApi.Repository
+{
+    /// <summary>
+    /// I recall that repositories are usually static, but after researching why,
+    /// I decided to make it none static so it can be tested in integration/unit test.
+    /// 
+    /// The feature to search by symbol or word isn't implemented yet, I am not sure how i would implement this yet on the front-end
+    /// </summary>
+    public class JapaneseWordRepository
+    {
+        //private Web context;
+        WebDemoEntities _context;
+        private EventLog appLog;
 
-//        public JapaneseWordRepository() 
-//        {
-//            //Data context is by default initialized
-//            context = new WebDemoDataContext();
+        public JapaneseWordRepository()
+        {
+            //Data context is by default initialized
+             _context = new WebDemoEntities();
 
-//            EventLog appLog = new EventLog();
-//            appLog.Source = "WebDemo";
-//            //appLog.WriteEntry("An entry to the Application event log.");
-//        }
+            EventLog appLog = new EventLog();
+            appLog.Source = "WebDemo";
+            //appLog.WriteEntry("An entry to the Application event log.");
+        }
 
-//        /// <summary>
-//        /// Get all entries from the table
-//        /// </summary>
-//        /// <returns></returns>
-//        public List<JapaneseWord> GetAllEntries() 
-//        {
-//            List<JapaneseWord> query = new List<JapaneseWord>();
-//            try
-//            {
+        /// <summary>
+        /// Get all entries from the table
+        /// </summary>
+        /// <returns></returns>
+        public List<JapaneseWord> GetAllEntries()
+        {
+            List<JapaneseWord> query = new List<JapaneseWord>();
+            try
+            {
 
-//                query = (from jwords in context.JapaneseWordEntries
-//                         select new JapaneseWord
-//                         {
-//                             EntryID =Convert.ToInt32( jwords.EntryId),
-//                             Hiragana = jwords.Hiragana,
-//                             Romaji = jwords.Romaji,
-//                             AdditionalText = jwords.AdditionalText,
-//                             Kanji = jwords.Kanji,
-//                             MotherTongueTranslation = jwords.MotherTongueTranslation,
-//                             MotherTongueTranslationLabel = jwords.MotherTongueTranslationLabel
-//                         }).ToList();
+                query = (from jwords in _context.JapaneseWordEntries
+                         select new JapaneseWord
+                         {
+                             EntryID = Convert.ToInt32(jwords.EntryId),
+                             Hiragana = jwords.Hiragana,
+                             Romaji = jwords.Romaji,
+                             AdditionalText = jwords.AdditionalText,
+                             Kanji = jwords.Kanji,
+                             MotherTongueTranslation = jwords.MotherTongueTranslation,
+                             MotherTongueTranslationLabel = jwords.MotherTongueTranslationLabel
+                         }).ToList();
 
-//            }
-//            catch (Exception ex)
-//            {
-//                appLog.WriteEntry(ex.Message);
-//            }
-//            return query;
-//        }
+            }
+            catch (Exception ex)
+            {
+                appLog.WriteEntry(ex.Message);
+            }
+            return query;
+        }
 
-//        /// <summary>
-//        /// Get one entry from the table
-//        /// </summary>
-//        /// <param name="id"></param>
-//        /// <returns></returns>
-//        public JapaneseWord GetEntry(int id) 
-//        {
+        /// <summary>
+        /// Get one entry from the table
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public JapaneseWord GetEntry(int id)
+        {
 
-//            JapaneseWord emptyModel = new JapaneseWord ();
+            JapaneseWord emptyModel = new JapaneseWord();
 
-//            try
-//            {
-               
-//                var query = (from entry in context.JapaneseWordEntries
-//                            where entry.EntryId == id
-//                            select entry).FirstOrDefault();
-//                JapaneseWord model = new JapaneseWord(query);
+            try
+            {
 
-//                return model;
-//            }
-//            catch (Exception ex)
-//            {
-//                appLog.WriteEntry(ex.Message);
-//            }
+                var query = (from entry in _context.JapaneseWordEntries
+                             where entry.EntryId == id
+                             select entry).FirstOrDefault();
+                JapaneseWord model = new JapaneseWord(query);
 
-//            return emptyModel;
-//        }
+                return model;
+            }
+            catch (Exception ex)
+            {
+                appLog.WriteEntry(ex.Message);
+            }
 
-//        /// <summary>
-//        /// Delete 1 entry from the table
-//        /// </summary>
-//        /// <param name="id"></param>
-//        public void DeleteEntry(int id)
-//        {
-//            try { 
-//            var jpword = (from words in context.JapaneseWordEntries
-//                                  where words.EntryId.Equals(id)
-//                                  select words).Single();
-            
-//            context.JapaneseWordEntries.DeleteOnSubmit(jpword);
-//            context.SubmitChanges();
-//        }
-//            catch(Exception ex)
-//            {
-//                appLog.WriteEntry(ex.Message);
-//            }
-//        }
+            return emptyModel;
+        }
 
-//        /// <summary>
-//        /// Add 1 entry to the table
-//        /// </summary>
-//        public void AddEntry(JapaneseWord model) 
-//        {
-//            if (model == null) 
-//            {
-//                throw new ArgumentNullException("model");
-//            }
-//            try
-//            {
-//                JapaneseWordEntry jpwe = new JapaneseWordEntry
-//                {
-//                    Hiragana = model.Hiragana,
-//                    Kanji = model.Kanji,
-//                    Romaji = model.Romaji,
-//                    AdditionalText = model.AdditionalText,
-//                    MotherTongueTranslation = model.MotherTongueTranslation,
-//                    MotherTongueTranslationLabel = model.MotherTongueTranslationLabel
-//                };
-//                jpwe.EntryId = null; // since this is an insert, id's are auto incremented on the database side
-//                context.JapaneseWordEntries.InsertOnSubmit(jpwe);
+        /// <summary>
+        /// Delete 1 entry from the table
+        /// </summary>
+        /// <param name="id"></param>
+        public void DeleteEntry(int id)
+        {
+            try
+            {
+                var jpword = (from words in _context.JapaneseWordEntries
+                              where words.EntryId.Equals(id)
+                              select words).Single();
 
-//                context.SubmitChanges();
-//            }
-//            catch (Exception ex)
-//            {
-//                appLog.WriteEntry(ex.Message);
-//            }
+                _context.JapaneseWordEntries.Remove(jpword);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                appLog.WriteEntry(ex.Message);
+            }
+        }
 
-//        }
+        /// <summary>
+        /// Add 1 entry to the table
+        /// </summary>
+        public void AddEntry(JapaneseWord model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentNullException("model");
+            }
+            try
+            {
+                JapaneseWordEntry jpwe = new JapaneseWordEntry
+                {
+                    Hiragana = model.Hiragana,
+                    Kanji = model.Kanji,
+                    Romaji = model.Romaji,
+                    AdditionalText = model.AdditionalText,
+                    MotherTongueTranslation = model.MotherTongueTranslation,
+                    MotherTongueTranslationLabel = model.MotherTongueTranslationLabel
+                };
+                 //jpwe.EntryId = 0; // since this is an insert, id's are auto incremented on the database side
+                _context.JapaneseWordEntries.Add(jpwe);
 
-//        /// <summary>
-//        /// Edit entry
-//        /// Retrieve model from database, overwrite the properties if the query if found
-//        /// </summary>
-//        public void EditEntry(JapaneseWord model) 
-//        {
-//            try
-//            {
-//                var query = (from entry in context.JapaneseWordEntries
-//                            where entry.EntryId == model.EntryID
-//                            select entry).FirstOrDefault();
-//                if (query != null)
-//                {
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                appLog.WriteEntry(ex.Message);
+            }
 
-//                    query.Romaji = model.Romaji;
-//                    query.Hiragana = model.Hiragana;
-//                    query.Kanji = model.Kanji;
-//                    query.AdditionalText = model.AdditionalText;
-//                    query.MotherTongueTranslation = model.MotherTongueTranslation;
-//                    query.MotherTongueTranslationLabel = model.MotherTongueTranslationLabel;
+        }
 
-//                    context.SubmitChanges();
-//                }
-//                else 
-//                {
-//                    throw new NullReferenceException("Entry model is null");
-//                }
+        /// <summary>
+        /// Edit entry
+        /// Retrieve model from database, overwrite the properties if the query if found
+        /// </summary>
+        public void EditEntry(JapaneseWord model)
+        {
+            try
+            {
+                var query = (from entry in _context.JapaneseWordEntries
+                             where entry.EntryId == model.EntryID
+                             select entry).FirstOrDefault();
+                if (query != null)
+                {
 
-//            }
-//            catch (Exception ex)
-//            {
-//                appLog.WriteEntry(ex.Message);
-//            }
+                    query.Romaji = model.Romaji;
+                    query.Hiragana = model.Hiragana;
+                    query.Kanji = model.Kanji;
+                    query.AdditionalText = model.AdditionalText;
+                    query.MotherTongueTranslation = model.MotherTongueTranslation;
+                    query.MotherTongueTranslationLabel = model.MotherTongueTranslationLabel;
 
-//        }
-        
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    throw new NullReferenceException("Entry model is null");
+                }
 
-//    }
-//}
+            }
+            catch (Exception ex)
+            {
+                appLog.WriteEntry(ex.Message);
+            }
+
+        }
+
+
+    }
+}
